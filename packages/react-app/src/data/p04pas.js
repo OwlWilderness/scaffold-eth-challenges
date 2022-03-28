9be1d50bf799027401780df712286914282397f4
OwlWilderness <98717833+OwlWilderness@users.noreply.github.com>
2022-03-28 17:19:12 +0000

    adding artist submission metadata

---
+++ packages/react-app/src/data/p04pas.js
@@ -0,0 +1,326 @@
+  // the json for the nfts
+  export const p04pasjson = {
+    1: {
+      description: "Artist Submission - 0201 01",
+      image: "https://ipfs.io/ipfs/QmaeFHMz34Q2DSdaTydt831bGc8X1Evy76RHem8Yn2kbhd?filename=P04PAS_0201_01.png" ,
+      name: "4020101",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    2: {
+      description: "Artist Submission - 0356 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmSPtEsiXqqunsHReF57weo2nV2aU8Jvv9xnH6nWNBfnM8?filename=P04PAS_0356_01.png" ,
+      name: "4035601",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    3: {
+      description: "Artist Submission - 1307 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmWF7wv1AVUQCTNMu6vSCXSz8PQwFWcHJf2pSPPYHmXZTW?filename=P04PAS_1307_01.png" ,
+      name: "4130701",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    4: {
+      description: "Artist Submission - 1325 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmS6PMgqqQJFDhBJnyHs5HFVV8ZE7abmHTaswLNHQSmNrJ?filename=P04PAS_1325_01.png" ,
+      name: "4132501",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    5: {
+      description: "Artist Submission - 1734 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmYoY2oHNGZ42UAtC2aM7G6YSN1Kat5HkjzUuNTiVnWr5R?filename=P04PAS_1734_01.png" ,
+      name: "4173401",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    6: {
+      description: "Artist Submission - 1734 02",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmWgp7e1KcuiWDFEdPo37XkFjWH2rpaW1TnmohwEuFwcKS?filename=P04PAS_1734_02.png" ,
+      name: "4173402",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    7: {
+      description: "Artist Submission - 1911 01 - GIF",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmahtMnwFeLD6HykEgP8qhjf5qt1ijkmVSfpF18Hg9xpp4?filename=P04PAS_1911_01.gif",
+      name: "4191101",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    8: {
+      description: "Artist Submission - 1954 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmSJzyM6bzC9fzUPitSTjWP2AWV3b3Nbr67hdAiJ4MDi2v?filename=P04PAS_1954_01.png" ,
+      name: "4195401",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    9: {
+      description: "Artist Submission - 2327 01 - GIF",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmaQVbinEVvQk9V4HhsFqErcDjqqAg4UaTVfiJz5LpU4o4?filename=P04PAS_2327_01.gif" ,
+      name: "4232701",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    10: {
+      description: "Artist Submission - 2327 02",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmQFAXp34bD5cHNqrNnZn574Nz8XEeTF7r2RTr4i4jcNJ3?filename=P04PAS_2327_02.png" ,
+      name: "4232702",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    11: {
+      description: "Artist Submission - 2710 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmYWwMThSC1KjHbfkXyubfix4vaz1EyFBGU9Ah1EBNrhVy?filename=P04PAS_2710_01.png" ,
+      name: "4271001",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    12: {
+      description: "Artist Submission - 3185 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmefqGgwSm9biyrVzAxqvZf8pRMvu2bJsFnb1YW4SptWFe?filename=P04PAS_3185_01.png" ,
+      name: "4318501",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    13: {
+      description: "Artist Submission - 4513 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmVRcYNttzLhxzsVGP7iKvgdgreeJY9ytHMpHEiVqpScjy?filename=P04PAS_4513_01.png" ,
+      name: "4451301",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    14: {
+      description: "Artist Submission - 5035 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmNsXsARRuUhoqpQfHxcp95gQqikcW1AVLEA7TkFGjpUQc?filename=P04PAS_5035_01.png" ,
+      name: "4503501",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    15: {
+      description: "Artist Submission - 5365 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmSQ3gcuEiwX8R8Ci5iF18oCqhUNUVxq1uFAc8bsAJBRM8?filename=P04PAS_5365_01.png" ,
+      name: "4536501",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    16: {
+      description: "Artist Submission - 6058 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmaNjtpCUucjE4deZJ2jBYzUjySxbY5jhPD8Sdw7pbccUF?filename=P04PAS_6058_01.png" ,
+      name: "4605801",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    17: {
+      description: "Artist Submission - 6058 02",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmX2X2DnoSfF2Pr5fCrGYwNT9mx9poy4b8NsgVjqVUT2za?filename=P04PAS_6058_02.png" ,
+      name: "4605802",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    18: {
+      description: "Artist Submission - 6058 03",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmR433PBuV7JSTcSuLRqdJvSvefvs9Szn73UVwhofsnnPR?filename=P04PAS_6058_03.png" ,
+      name: "4605803",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    19: {
+      description: "Artist Submission - 6846 01 - GIF",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmSojQeFtyYmCiJW8BFVK3vgEL6h1d89zYG6vVbqY591s2?filename=P04PAS_6846_01.gif" ,
+      name: "4684601",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    20: {
+      description: "Artist Submission - 7408 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmWdayuQhWqDS2qTVn8a85ejdP687bnhnrM92twbQYsN7D?filename=P04PAS_7408_01.png" ,
+      name: "4740801",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    21: {
+      description: "Artist Submission - 7524 01 - GIF",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/Qmb8kawHhRrGwyfVhe3P8Ge5adyUCmhqC6HfX3NyrHUdid?filename=P04PAS_7524_01.gif" ,
+      name: "4752401",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    22: {
+      description: "Artist Submission - 7719 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmaWgbPAuEw3dh2ZWKYYx9y6DS34YCy4MaRqpzDz8RghRS?filename=P04PAS_7719_01.png" ,
+      name: "4771901",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    23: {
+      description: "Artist Submission - 8002 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmTfWx6rd7hwUZtRENH81tornyiVCZWhiAhChMiRVaFzH3?filename=P04PAS_8002_01.png" ,
+      name: "4800201",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    24: {
+      description: "Artist Submission - 8909 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmZ9Evsv9eDpEpUaGR34wkU6CjPjx6rBoBj2XWRERd6E2m?filename=P04PAS_8909_01.png" ,
+      name: "4890901",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    25: {
+      description: "Artist Submission - 8909 02",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmY9WitGuzUe8arLTvuiFJ9z6FtSpD9jz436KSfJeeJctk?filename=P04PAS_8909_02.png" ,
+      name: "4890902",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },  
+    26: {
+      description: "Artist Submission - 9499 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmcfSHbJFJEm2dMTyzAjnyXqLxBpPMS2BcNYEMKFz3tToF?filename=P04PAS_9499_01.png" ,
+      name: "4949901",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },
+    27: {
+      description: "Artist Submission - 9567 01",
+      external_url: "https://ipfs.io/ipfs/",
+      image: "https://ipfs.io/ipfs/QmP3Hyqd5qizWUmSWWqjNxFV8a31dwuNG2yxETgg299SHf?filename=P04PAS_9567_01.png" ,
+      name: "4956701",
+      attributes: [
+        {
+          trait_type: "POAPAthon Bounty",
+          value: "POAP 4 PEACE"
+        },
+      ],
+    },  
+  };
